05d8f25c56256490dddff7a318379216289f5637
Anton <Feverqwe@users.noreply.github.com>
2018-05-25 19:25:13 +0500

    getScriptsFormHtml parse5

--- builder/getScriptsFromHtml.js
+++ builder/getScriptsFromHtml.js
@@ -1,9 +1,24 @@
+const parse5 = require('parse5');
+
 const getScriptsFormHtml = html => {
-  const m = /src="(.+)"/g.exec(html);
-  if (m) {
-    m.shift();
-  }
-  return m || [];
+  const scripts = [];
+
+  const ast = parse5.parse(html);
+  const walk = node => {
+    if (node.tagName === 'script') {
+      node.attrs && node.attrs.some(attr => {
+        if (attr.name === 'src') {
+          scripts.push(attr.value);
+        }
+      });
+    } else
+    if (node.childNodes) {
+      node.childNodes.forEach(walk);
+    }
+  };
+  walk(ast);
+
+  return scripts;
 };
 
 module.exports = getScriptsFormHtml;
