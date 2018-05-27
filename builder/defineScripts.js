2cb3b02ce99069cb2a02df2d5925717c3106fd19
Anton <leonardspbox@gmail.com>
2018-05-27 19:30:25 +0500

    defineScripts via toString

--- builder/defineScripts.js
+++ builder/defineScripts.js
@@ -1,8 +1,12 @@
 const defineScripts = scripts => {
+  // DO NOT REPLACE VAR, env don't fallback it
+  const toString = function(){
+    var string = this.f.toString();
+    var pos = string.indexOf('{') + 1;
+    return string.substr(pos, string.lastIndexOf('}') - pos);
+  };
   return `[${scripts.map(script => {
-    const fn = new Function(script);
-    fn.name = '';
-    return fn;
+    return `{f:${new Function(script).toString()},toString:${toString}}`;
   }).join(',')}]`;
 };
 
--- src/vendor/mono/unwrapScript.js
+++
@@ -1,8 +0,0 @@
-const unwrapScript = fn => {
-  let string = fn.toString();
-  const pos = string.indexOf('{') + 1;
-  string = string.substr(pos, string.lastIndexOf('}') - pos);
-  return string;
-};
-
-export default unwrapScript;
