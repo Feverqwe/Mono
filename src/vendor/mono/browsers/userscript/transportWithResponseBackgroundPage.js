fa41ef0620211096fdbece6cea5ab3934828899b
Anton <leonardspbox@gmail.com>
2018-05-27 10:16:25 +0500

    1

--- src/vendor/mono/browsers/userscript/transportWithResponseBackgroundPage.js
+++ src/vendor/mono/browsers/userscript/transportWithResponseBackgroundPage.js
@@ -13,16 +13,14 @@ class TransportWithResponseBackgroundPage extends TransportWithResponseWithActiv
   }
   callListeners(message, sender, response) {
     let result = super.callListeners(message, sender, response);
-    if (result !== true) {
-      this.bundle.monoInstances.forEach(mono => {
-        if (mono.transport instanceof TransportWithResponsePage) {
-          const r = mono.transport.callListeners(message, sender, response);
-          if (r === true) {
-            result = true;
-          }
+    this.bundle.monoInstances.forEach(mono => {
+      if (mono.transport instanceof TransportWithResponsePage) {
+        const r = mono.transport.callListeners(message, sender, response);
+        if (r === true) {
+          result = true;
         }
-      });
-    }
+      }
+    });
     return result;
   }
 }
