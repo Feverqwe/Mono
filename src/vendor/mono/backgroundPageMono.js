b0f2e934cc29f4073c299d236059ff2c7a8e884f
Anton <leonardspbox@gmail.com>
2018-05-29 18:36:23 +0500

    1

--- src/vendor/mono/backgroundPageMono.js
+++ src/vendor/mono/backgroundPageMono.js
@@ -2,12 +2,19 @@ import Mono from "./mono";
 import BackgroundPageCallFn from "./backgroundPageCallFn";
 import PageMonoMixin from "./pageMonoMixin";
 
+const notImplemented = () => {
+  throw new Error('Unimplemented api');
+};
+
 class BackgroundPageMono extends PageMonoMixin(Mono) {
   initMessages() {
     super.initMessages();
 
     this.backgroundPageCallFn = new BackgroundPageCallFn(this);
     this.remote = this.backgroundPageCallFn.remote;
+  }
+  openTab(url, active) {
+    notImplemented();
   }
 }
 
