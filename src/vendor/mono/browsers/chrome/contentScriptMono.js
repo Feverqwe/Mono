cc61dc5941a4e810f6ff5eee0bf000c79488e18d
Anton <Feverqwe@users.noreply.github.com>
2018-05-29 20:33:37 +0500

    chrome messages, storage

--- src/vendor/mono/browsers/chrome/contentScriptMono.js
+++ src/vendor/mono/browsers/chrome/contentScriptMono.js
@@ -1,7 +1,44 @@
 import ContentScriptMono from "../../contentScriptMono";
+import Storage from "../../storage";
 
 class ChromeContentScriptMono extends ContentScriptMono {
+  initMessages() {
+    this.transport = {
+      sendMessage(message, response) {
+        chrome.runtime.sendMessage(message, response);
+      },
+      addListener(listener) {
+        chrome.runtime.onMessage.addListener(listener);
+      },
+      hasListener(listener) {
+        return chrome.runtime.onMessage.hasListener(listener);
+      },
+      hasListeners() {
+        return chrome.runtime.onMessage.hasListeners();
+      },
+      removeListener(listener) {
+        chrome.runtime.onMessage.removeListener(listener);
+      }
+    };
 
+    super.initMessages();
+  }
+  initStorage() {
+    this.storage = new Storage({
+      get(keys, callback) {
+        chrome.storage.local.get(keys, callback);
+      },
+      set(data, callback) {
+        chrome.storage.local.set(data, callback);
+      },
+      remove(keys, callback) {
+        chrome.storage.local.remove(keys, callback);
+      },
+      clear(callback) {
+        chrome.storage.local.clear(callback);
+      }
+    });
+  }
 }
 
 export default ChromeContentScriptMono;
--- src/vendor/mono/browsers/chrome/pageMonoMixin.js
+++ src/vendor/mono/browsers/chrome/pageMonoMixin.js
@@ -1,5 +1,54 @@
-const ChromePageMonoMixin = Parent => class extends Parent {
+import Storage from "../../storage";
 
+const ChromePageMonoMixin = Parent => class extends Parent {
+  initMessages() {
+    this.transport = {
+      sendMessage(message, response) {
+        chrome.runtime.sendMessage(message, response);
+      },
+      sendMessageToActiveTab(message, response) {
+        chrome.tabs.query({
+          active: true,
+          currentWindow: true
+        }, tabs => {
+          const tabId = tabs[0] && tabs[0].id;
+          if (tabId >= 0) {
+            chrome.tabs.sendMessage(tabId, message, response);
+          }
+        });
+      },
+      addListener(listener) {
+        chrome.runtime.onMessage.addListener(listener);
+      },
+      hasListener(listener) {
+        return chrome.runtime.onMessage.hasListener(listener);
+      },
+      hasListeners() {
+        return chrome.runtime.onMessage.hasListeners();
+      },
+      removeListener(listener) {
+        chrome.runtime.onMessage.removeListener(listener);
+      }
+    };
+
+    super.initMessages();
+  }
+  initStorage() {
+    this.storage = new Storage({
+      get(keys, callback) {
+        chrome.storage.local.get(keys, callback);
+      },
+      set(data, callback) {
+        chrome.storage.local.set(data, callback);
+      },
+      remove(keys, callback) {
+        chrome.storage.local.remove(keys, callback);
+      },
+      clear(callback) {
+        chrome.storage.local.clear(callback);
+      }
+    });
+  }
 };
 
 export default ChromePageMonoMixin;
