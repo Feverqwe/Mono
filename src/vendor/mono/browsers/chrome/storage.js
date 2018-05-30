cfa91108add9260441663a7899d60d5518166cd7
Anton <leonardspbox@gmail.com>
2018-05-30 10:50:41 +0500

    1

--- src/vendor/mono/browsers/chrome/storage.js
+++ src/vendor/mono/browsers/chrome/storage.js
@@ -2,33 +2,24 @@ class ChromeStorage {
   constructor(mono) {
     this.mono = mono;
   }
-  get(keys, callback) {
-    chrome.storage.local.get(keys, result => {
-      this.mono.lastError = chrome.runtime.lastError;
+  callback(callback, result, notOptionalCallback) {
+    this.mono.lastError = chrome.runtime.lastError;
+    if (notOptionalCallback || callback) {
       callback(result);
-      this.mono.clearLastError();
-    });
+    }
+    this.mono.clearLastError();
+  }
+  get(keys, callback) {
+    chrome.storage.local.get(keys, result => this.callback(callback, result, true));
   }
   set(items, callback) {
-    chrome.storage.local.set(items, () => {
-      this.mono.lastError = chrome.runtime.lastError;
-      callback && callback();
-      this.mono.clearLastError();
-    });
+    chrome.storage.local.set(items, () => this.callback(callback));
   }
   remove(keys, callback) {
-    chrome.storage.local.remove(keys, () => {
-      this.mono.lastError = chrome.runtime.lastError;
-      callback && callback();
-      this.mono.clearLastError();
-    });
+    chrome.storage.local.remove(keys, () => this.callback(callback));
   }
   clear(callback) {
-    chrome.storage.local.clear(() => {
-      this.mono.lastError = chrome.runtime.lastError;
-      callback && callback();
-      this.mono.clearLastError();
-    });
+    chrome.storage.local.clear(() => this.callback(callback));
   }
 }
 
