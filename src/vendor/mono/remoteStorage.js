c7773064553c384f87168da481af4e1b69b77d32
Anton <leonardspbox@gmail.com>
2018-05-30 10:54:12 +0500

    1

--- src/vendor/mono/remoteStorage.js
+++ src/vendor/mono/remoteStorage.js
@@ -4,40 +4,33 @@ class RemoteStorage {
   constructor(mono) {
     this.mono = mono;
   }
+  onError(callback, err, notOptionalCallback) {
+    this.mono.lastError = err;
+    if (notOptionalCallback || callback) {
+      callback();
+    }
+    this.mono.clearLastError();
+  }
   get(keys, callback) {
     this.mono.callFn('mono.storage.remote.get', [wrapObjectValues(keys)]).then(result => {
       callback(unwrapObjectValues(result));
     }, err => {
-      this.mono.lastError = err;
-      callback();
-      this.mono.clearLastError();
+      this.onError(callback, err, true);
     });
   }
   set(items, callback) {
-    this.mono.callFn('mono.storage.remote.set', [wrapObjectValues(items)]).then(() => {
-      callback && callback();
-    }, err => {
-      this.mono.lastError = err;
-      callback && callback();
-      this.mono.clearLastError();
+    this.mono.callFn('mono.storage.remote.set', [wrapObjectValues(items)]).then(callback, err => {
+      this.onError(callback, err);
     });
   }
   remove(keys, callback) {
-    this.mono.callFn('mono.storage.remote.remove', [keys]).then(() => {
-      callback && callback();
-    }, err => {
-      this.mono.lastError = err;
-      callback();
-      this.mono.clearLastError();
+    this.mono.callFn('mono.storage.remote.remove', [keys]).then(callback, err => {
+      this.onError(callback, err);
     });
   }
   clear(callback) {
-    this.mono.callFn('mono.storage.remote.clear').then(() => {
-      callback && callback();
-    }, err => {
-      this.mono.lastError = err;
-      callback();
-      this.mono.clearLastError();
+    this.mono.callFn('mono.storage.remote.clear').then(callback, err => {
+      this.onError(callback, err);
     });
   }
 }
