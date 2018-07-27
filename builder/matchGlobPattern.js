a2aaa9c355bd29a1a690f0f79292a2943534afa3
Anton <leonardspbox@gmail.com>
2018-07-27 06:38:56 +0500

    matchGlobPattern upd

--- builder/matchGlobPattern.js
+++ builder/matchGlobPattern.js
@@ -1,10 +1,30 @@
 const escapeStringRegexp = require('escape-string-regexp');
 
+const getScheme = scheme => {
+  return escapeStringRegexp(scheme.toLowerCase()).replace(/\\\*/g, '[^:]*') + '\/\/';
+};
+
+const getPath = path => {
+  return escapeStringRegexp(path.toLowerCase()).replace(/\\\*/g, '.*');
+};
+
+const hostnameToRePattern = (scheme, hostname, path) => {
+  return '^' + getScheme(scheme) + escapeStringRegexp(hostname.toLowerCase()).replace(/\\\*/g, '[^\\/]*') + getPath(path) + '$';
+};
+
 const matchGlobPattern = pattern => {
-  let rePattern = escapeStringRegexp(pattern);
-  rePattern = rePattern.replace(/\\\*/g, '.*');
-  rePattern = rePattern.replace(/\\\?/g, '.{1}');
-  return ['^' + rePattern + '$'];
+  const m = /^([^:\/]+:)\/\/([^\/]*)(\/.*)/.exec(pattern);
+  if (m) {
+    const scheme = m[1];
+    const hostname = m[2];
+    const path = m[3];
+    return [hostnameToRePattern(scheme, hostname, path).replace(/\\\?/g, '.{1}')];
+  } else {
+    let rePattern = escapeStringRegexp(pattern);
+    rePattern = rePattern.replace(/\\\*/g, '.*');
+    rePattern = rePattern.replace(/\\\?/g, '.{1}');
+    return ['^' + rePattern + '$'];
+  }
 };
 
 module.exports = matchGlobPattern;
